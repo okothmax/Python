1def353705a8be8b880390752c986d613d25442d
okothmax <okothmaxwell03@gmail.com>
2022-11-24 11:11:25 +0300

    youtube module fix

--- youtube.py
+++ youtube.py
@@ -1,10 +1,11 @@
-from pytube import YouTube
+import os
+import youtube
 # define where the video will be stored
 Download_Directory = "/Users/Orain/Dowloads/Video"
 # the user will enter the download link
-video_url = input("youtube.com/....")
+video_url = input("enter the url: ")
 # creating an instance of the video
-video_obj = YouTube(video_url)
+video_obj = youtube(video_url)
 stream = video_obj.stream.get_highest_resolution()
 # download the video into the prescribed path
 stream.download(Download_Directory)
